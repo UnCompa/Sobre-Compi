44356aeedb8b2976e52becc8c660b93dc59371b3
UnCompa <125362398+UnCompa@users.noreply.github.com>
2024-01-27 13:18:08 -0500

    Pequeños cambios a CardRecuerdos.jsx

--- src/components/CardRecuerdos.jsx
+++ src/components/CardRecuerdos.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Recuerdos({ imagen, frase }) {
   return (
     <motion.article
-      initial={{ opacity: 0, y: 100 }}
+      initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       className="bg-zinc-800 bg-opacity-50 border-2 border-pastel-3 h-full w-full p-4 rounded flex-col gap-4 shadow-lg"
     >
@@ -12,9 +12,10 @@ export default function Recuerdos({ imagen, frase }) {
           src={imagen}
           alt="Imagen"
           className="h-full w-full object-cover rounded"
+          loading="lazy"
         />
       </div>
-      <div className="p-4 font-Poppins text-xl text-white font-bold">
+      <div className="bg-zinc-900 px-2 py-4 font-Poppins text-xl text-white font-bold">
         <p>{frase}</p>
       </div>
     </motion.article>
