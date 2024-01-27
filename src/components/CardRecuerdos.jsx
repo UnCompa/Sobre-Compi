98bb21f07db8e4177dec4d5e0fc463ec684d806a
UnCompa <125362398+UnCompa@users.noreply.github.com>
2024-01-27 12:38:56 -0500

    Cambios para CardRecuerdos.jsx

--- src/components/CardRecuerdos.jsx
+++ src/components/CardRecuerdos.jsx
@@ -5,13 +5,13 @@ export default function Recuerdos({ imagen, frase }) {
     <motion.article
       initial={{ opacity: 0, y: 100 }}
       whileInView={{ opacity: 1, y: 0 }}
-      className="bg-zinc-500 bg-opacity-50 border-2 border-pastel-3 h-full w-full p-2 rounded flex-col gap-4 shadow-lg"
+      className="bg-zinc-800 bg-opacity-50 border-2 border-pastel-3 h-full w-full p-4 rounded flex-col gap-4 shadow-lg"
     >
       <div>
         <img
           src={imagen}
           alt="Imagen"
-          className="h-full w-full object-cover bg-red-500 rounded"
+          className="h-full w-full object-cover rounded"
         />
       </div>
       <div className="p-4 font-Poppins text-xl">
