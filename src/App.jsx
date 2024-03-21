7aa3d1a4903d7a8ac6676ec8894a03844ca1f1e3
UnCompa <brandonddxd@gmail.com>
2024-03-21 18:21:31 -0500

    Se agrego un form para imagenes con las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import Home from "./pages/home";
 import Info from './pages/info'
 import NoFound from './pages/nofound'
 import { Routes, Route } from "react-router-dom";
-import Recuerdos from './pages/recuerdos'
+import Notas from "./pages/notas";
+import Recuerdos2 from "./pages/recuerdos2";
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/informacion" element={<Info/>}/>
-      <Route path="/recuerdos" element={<Recuerdos/>}/>
+      <Route path="/recuerdos" element={<Recuerdos2/>}/>
+      <Route path="/notas" element={<Notas/>}/>
+      <Route path="/notas" element={<Notas/>}/>
       <Route path="/*" element={<NoFound/>}/>
       <Route path="*" element={<NoFound/>}/>
     </Routes>
--- src/components/CardRecuerdos.jsx
+++ src/components/CardRecuerdos.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
-export default function Recuerdos({ imagen, frase }) {
+export default function Recuerdos({ imagen, frase, title, id }) {
+  const handleClick = () =>{
+    const api = `https://compis.onrender.com/api/images/${id}`
+    fetch(api,{
+      method: "DELETE"
+    }).then((res)=> res.json())
+    .then((data)=>console.log(data))
+  }
   return (
     <motion.article
       initial={{ opacity: 0, y: 100 }}
       whileInView={{ opacity: 1, y: 0 }}
-      className="bg-zinc-500 bg-opacity-50 border-2 border-pastel-3 h-full w-full p-2 rounded flex-col gap-4 shadow-lg"
+      className="bg-zinc-50 border-2 border-pastel-3 h-full w-full p-2 rounded flex-col gap-4 shadow-lg"
     >
       <div>
         <img
@@ -14,8 +21,14 @@ export default function Recuerdos({ imagen, frase }) {
           className="h-full w-full object-cover bg-red-500 rounded"
         />
       </div>
-      <div className="p-4 font-Poppins text-xl">
+      <div className="text-center text-2xl font-Poppins font-bold text-black">
+        <h3>{title}</h3>
+      </div>
+      <div className="px-2 font-Poppins text-base">
         <p>{frase}</p>
+      </div>
+      <div>
+        <button onClick={handleClick} className="bg-red-500 px-4 py-1 mx-2 my-2 rounded-md text-white font-bold">Borrar</button>
       </div>
     </motion.article>
   );
