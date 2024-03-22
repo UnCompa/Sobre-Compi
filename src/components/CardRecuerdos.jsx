dfe7d9cdee9204ca8b8f3d76e9ed5540abd475d6
UnCompa <125362398+UnCompa@users.noreply.github.com>
2024-03-21 19:33:12 -0500

    Update2 CardRecuerdos.jsx

--- src/components/CardRecuerdos.jsx
+++ src/components/CardRecuerdos.jsx
@@ -14,7 +14,7 @@ export default function Recuerdos({ imagen, frase, title, id }) {
   return (
     <motion.article
       initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 
+      whileInView={{ opacity: 1, y: 0}}
       className="bg-zinc-800 bg-opacity-50 border-2 border-pastel-3 h-full w-full p-4 rounded flex-col gap-4 shadow-lg"
     >
       <div>
